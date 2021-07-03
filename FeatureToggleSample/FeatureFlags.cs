d9d206b6b01faecf68ea39738e887c625d9a8c5d
Adem Catamak <ademcatamak@gmail.com>
2021-07-03 16:44:56 +0300

    Feature Toggle Management Sample

---
+++ FeatureToggleSample/FeatureFlags.cs
@@ -0,0 +1,10 @@
+namespace FeatureToggleSample
+{
+    public class FeatureFlags
+    {
+        public const string EXTERNAL_SYSTEM_INTEGRATION = "ExternalSystemIntegration";
+        public const string USE_UTC = "UseUtc";
+        public const string PREVIEW_ACTIVE = "PreviewActive";
+
+    }
+}
---
+++ FeatureToggleSample/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.FeatureManagement;
+
+namespace FeatureToggleSample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IFeatureManager _featureManager;
+
+        public string ExternalSystemIntegrationStatus = "unknown";
+        public DateTime Now;
+        public bool IsPreviewActive;
+
+
+        public IndexModel(IFeatureManager featureManager)
+        {
+            _featureManager = featureManager;
+        }
+
+        public async Task OnGet()
+        {
+            bool isExternalSystemActive = await _featureManager.IsEnabledAsync(FeatureFlags.EXTERNAL_SYSTEM_INTEGRATION);
+            ExternalSystemIntegrationStatus = isExternalSystemActive ? "active" : "inactive";
+
+            bool isUtcActive = await _featureManager.IsEnabledAsync(FeatureFlags.USE_UTC);
+            if (isUtcActive)
+                Now = DateTime.UtcNow;
+            else
+                Now = DateTime.Now;
+
+            IsPreviewActive = await _featureManager.IsEnabledAsync(FeatureFlags.PREVIEW_ACTIVE);
+        }
+    }
+}
